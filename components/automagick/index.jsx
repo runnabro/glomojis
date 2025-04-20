3a900efcd46100f6644172dabe4d5c99b859a056
Tony Li <runnabro@users.noreply.github.com>
2025-04-20 11:52:56 -0700

    more stuff

--- components/automagick/index.jsx
+++ components/automagick/index.jsx
@@ -1,17 +1,18 @@
 "use client";
 
 import { useEffect, useState } from "react";
-
 import {
   initializeImageMagick,
   ImageMagick,
   Magick,
   MagickFormat,
 } from "@imagemagick/magick-wasm";
+import { FileImage, Loader } from "lucide-react";
 
 import classes from "./style.module.scss";
 
 const AutoMagick = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [inputFile, setInputFile] = useState(null);
   const [outputFile, setOutputFile] = useState(null);
   const [isInitialized, setIsInitialized] = useState(false);
@@ -40,6 +41,15 @@ const AutoMagick = () => {
   }
 
   const handleFileChange = async (e) => {
+    setIsLoading(true);
+
+    // abort
+    if (e.target.files.length === 0) {
+      setIsLoading(false);
+      return;
+    }
+
+    // proceed
     const file = e.target.files[0];
     if (file) {
       const url = URL.createObjectURL(file);
@@ -51,16 +61,19 @@ const AutoMagick = () => {
 
   const processImage = async (file) => {
     if (!isInitialized) {
-      console.log("ImageMagick not initialized yet");
+      setIsLoading(false);
+      console.log("ImageMagick not Initialized");
       return;
     }
 
     return new Promise((resolve, reject) => {
       (async () => {
         try {
+          console.log("Quantum Formatting");
           // Set quantum format first
           Magick.quantumFormat = "floating-point";
 
+          console.log("Loading profile");
           // Load the profile first
           const profileResponse = await fetch("/2020_profile.icc");
           const profileData = await profileResponse.arrayBuffer();
@@ -68,6 +81,7 @@ const AutoMagick = () => {
 
           ImageMagick.read(file, (image) => {
             try {
+              console.log("Applying HDR");
               // Apply hdr
               image.colorspace = "RGB";
               image.autoGamma();
@@ -76,20 +90,26 @@ const AutoMagick = () => {
               image.colorspace = "sRGB";
               image.depth = 16;
 
+              console.log("Applying Profile");
               // Apply profile
               image.setProfile("icc", profileBytes);
 
+              console.log("Generating Image");
               // Write the result
               image.write(MagickFormat.Png, (data) => {
                 const blob = new Blob([data], { type: "image/png" });
                 setOutputFile(URL.createObjectURL(blob));
                 resolve(blob);
               });
+
+              setIsLoading(false);
             } catch (error) {
+              setIsLoading(false);
               reject(error);
             }
           });
         } catch (error) {
+          setIsLoading(false);
           reject(error);
         }
       })();
@@ -97,21 +117,51 @@ const AutoMagick = () => {
   };
 
   const doMagick = async () => {
-    if (!inputFile) return;
+    if (!inputFile) {
+      setIsLoading(false);
+      return;
+    }
     const imageData = await getImage(inputFile);
     await processImage(imageData);
   };
 
   return (
-    <>
-      <input
-        accept="image/png, image/jpeg, image/webp, image/gif"
-        onChange={handleFileChange}
-        type="file"
-      />
-      <img className={classes["AutoMagick-img"]} src={inputFile} />
-      <img className={classes["AutoMagick-img"]} src={outputFile} />
-    </>
+    <div className={classes["AutoMagick"]}>
+      <label
+        className={`${classes["AutoMagick-upload"]} ${
+          isLoading ? classes["AutoMagick-upload_disabled"] : ""
+        }`}
+      >
+        <input
+          accept="image/png, image/jpeg, image/webp, image/gif, image/heic"
+          className={classes["AutoMagick-upload-input"]}
+          disabled={isLoading}
+          id="hdr-input"
+          onChange={handleFileChange}
+          type="file"
+        />
+        Choose image to HDR-infuse
+        {isLoading ? (
+          <Loader className={classes["AutoMagick-upload-icon"]} />
+        ) : (
+          <>
+            {inputFile ? (
+              <img
+                className={classes["AutoMagick-upload-preview"]}
+                src={inputFile}
+              />
+            ) : (
+              <FileImage className={classes["AutoMagick-upload-icon"]} />
+            )}
+          </>
+        )}
+      </label>
+      <div className={classes["AutoMagick-output"]}>
+        {!isLoading && (
+          <img className={classes["AutoMagick-img"]} src={outputFile} />
+        )}
+      </div>
+    </div>
   );
 };
 
