a9b4e81a1956f009a0c9119cab2fe78895fb8340
Tony Li <runnabro@users.noreply.github.com>
2025-04-20 13:52:18 -0700

    add images

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
 import HdrInfuser from "../components/HdrInfuser";
-
+import Gallery from "../components/Gallery";
 export default function Home() {
   return (
     <main>
       <HdrInfuser />
+      <Gallery />
     </main>
   );
 }
---
+++ components/Gallery/index.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+
+// import * as Emojis from "./src";
+import classes from "./style.module.scss";
+
+const Gallery = () => {
+  const Images = () => {
+    // const imageList = Object.keys(Emojis);
+    return (
+      <>
+        {imageList?.map((image) => (
+          <tr key={`emoji-${image}`}>
+            <td>{image}</td>
+            <td>
+              <a download href={Emojis[image]}>
+                <Image alt={image} height={25} src={Emojis[image]} width={25} />
+              </a>
+            </td>
+          </tr>
+        ))}
+      </>
+    );
+  };
+  return (
+    <article className={classes.Gallery}>
+      <table>
+        <tbody>
+          <Images />
+        </tbody>
+      </table>
+    </article>
+  );
+};
+
+export default Gallery;
